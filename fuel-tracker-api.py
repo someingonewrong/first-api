b9940937fb486382b7d0192efede640acacb467d
Alec Brombley <alecbrombley00@gmail.com>
2024-06-10 21:21:56 +0100

    Setup base pages with navigation list

--- fuel-tracker-api.py
+++ fuel-tracker-api.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('index.html')
+@app.route('/New_Entry')
+def New_Input():
+    return render_template('New_Entry.html')
+
+@app.route('/Import_CSV')
+def CSV_Import():
+    return render_template('Import_CSV.html')
+
+@app.route('/View_Data')
+def View_Data():
+    return render_template('View_Data.html')
+
+@app.route('/SQL_Query')
+def SQL_Query():
+    return render_template('SQL_Query.html')
